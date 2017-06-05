7040ff2d15b43bc3be4745db077982f2f83eddfd
Pavithranagu <pavi.rachu@gmail.com>
2017-06-05 21:39:10 +0530

    Create ore.java

---
+++ ore.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class oddoreven
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n%2==0)
+{
+System.out.print("Even");
+}
+else
+{
+System.out.print("Odd");
+}
+}}
